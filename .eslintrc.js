030d54eb76b601dde6706215cf8ff1db0e7104ba
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-23 17:24:41 +0200

    extend eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 const eslintSetup = require('@papillonbits/setup/eslint')
 
-module.exports = { ...eslintSetup }
+module.exports = {
+  ...eslintSetup,
+  rules: {
+    ...eslintSetup.rules,
+    'no-console': 'off',
+    'no-restricted-syntax': 'off',
+    'no-sparse-arrays': 'off',
+  },
+}
