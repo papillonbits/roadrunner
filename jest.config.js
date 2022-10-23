f59c6a109f87a691545d6dd2eb73ec4e8df04945
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-23 16:02:08 +0200

    setup jest

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+// https://docs.codecov.com/docs/codecov-uploader
+/* eslint-disable prefer-destructuring */
+const getJestSetup = require('@papillonbits/setup/jest').getJestSetup
+
+const testPathIgnorePatterns = []
+const coverageDirectory = './.coverage/'
+const collectCoverage = true
+const collectCoverageFrom = [
+  '!**/.coverage/**',
+  '!**/.husky/**',
+  '!**/.mock/**',
+  '!**/.storybook/**',
+  '!**/.storybook-static/**',
+  '!**/node_modules/**',
+  'src/**/**/*.{js,jsx}',
+  '!src/**/**/*.story.js',
+  '!src/**/**/*.test.js',
+]
+const coverageThreshold = {
+  global: {
+    statements: 100,
+    branches: 100,
+    functions: 100,
+    lines: 100,
+  },
+}
+
+module.exports = getJestSetup({ testPathIgnorePatterns, coverageDirectory, collectCoverage, collectCoverageFrom, coverageThreshold })
