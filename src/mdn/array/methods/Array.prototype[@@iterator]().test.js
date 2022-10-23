7735345bb59296f301d865e7290c570b4b5e138d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-23 16:11:23 +0200

    test Array.prototype[@@iterator]()

---
+++ src/mdn/array/methods/Array.prototype[@@iterator]().test.js
@@ -0,0 +1,30 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/@@iterator
+
+describe('Array', () => {
+  let consoleLogSpy
+
+  beforeEach(() => {
+    consoleLogSpy = jest.fn(() => {})
+    jest.spyOn(console, 'log').mockImplementation(consoleLogSpy)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('prototype[@@iterator]()', () => {
+    const arr = ['a', 'b', 'c', 'd', 'e']
+    const arrIter = arr[Symbol.iterator]()
+
+    for (const letter of arrIter) {
+      console.log(letter)
+    }
+
+    expect(consoleLogSpy).toHaveBeenCalledTimes(5)
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 'a')
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(2, 'b')
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(3, 'c')
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(4, 'd')
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(5, 'e')
+  })
+})
