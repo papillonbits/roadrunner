75d92ea7c66531c039f55be2521528579ddf979e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-23 17:23:18 +0200

    extend Array.prototype[@@iterator]() tests

--- src/mdn/array/methods/Array.prototype.[@@iterator]().test.js
+++ src/mdn/array/methods/Array.prototype.[@@iterator]().test.js
@@ -1,30 +1,62 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/@@iterator
 
 describe('Array', () => {
-  let consoleLogSpy
+  describe('prototype.[@@iterator]()', () => {
+    let consoleLogSpy
 
-  beforeEach(() => {
-    consoleLogSpy = jest.fn(() => {})
-    jest.spyOn(console, 'log').mockImplementation(consoleLogSpy)
-  })
+    beforeEach(() => {
+      consoleLogSpy = jest.fn(() => {})
+      jest.spyOn(console, 'log').mockImplementation(consoleLogSpy)
+    })
 
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
 
-  test('prototype.[@@iterator]()', () => {
-    const arr = ['a', 'b', 'c', 'd', 'e']
-    const arrIter = arr[Symbol.iterator]()
+    test('Manually hand-rolling the iterator', () => {
+      const arr = ['a', 'b', 'c', 'd', 'e']
+      const arrIter = arr[Symbol.iterator]()
 
-    for (const letter of arrIter) {
-      console.log(letter)
-    }
+      for (const letter of arrIter) {
+        console.log(letter)
+      }
 
-    expect(consoleLogSpy).toHaveBeenCalledTimes(5)
-    expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 'a')
-    expect(consoleLogSpy).toHaveBeenNthCalledWith(2, 'b')
-    expect(consoleLogSpy).toHaveBeenNthCalledWith(3, 'c')
-    expect(consoleLogSpy).toHaveBeenNthCalledWith(4, 'd')
-    expect(consoleLogSpy).toHaveBeenNthCalledWith(5, 'e')
+      expect(consoleLogSpy).toHaveBeenCalledTimes(5)
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 'a')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(2, 'b')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(3, 'c')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(4, 'd')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(5, 'e')
+    })
+
+    test('Handling strings and string arrays with the same function', () => {
+      function logIterable(it) {
+        if (!(typeof it[Symbol.iterator] === 'function')) {
+          console.log(`${it} is not an iterable object.`)
+          return
+        }
+
+        for (const letter of it) {
+          console.log(letter)
+        }
+      }
+
+      logIterable(['a', 'b', 'c'])
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 'a')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(2, 'b')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(3, 'c')
+
+      consoleLogSpy.mockReset()
+
+      logIterable('abc')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 'a')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(2, 'b')
+      expect(consoleLogSpy).toHaveBeenNthCalledWith(3, 'c')
+
+      consoleLogSpy.mockReset()
+
+      logIterable(123)
+      expect(consoleLogSpy).toHaveBeenCalledWith('123 is not an iterable object.')
+    })
   })
 })
