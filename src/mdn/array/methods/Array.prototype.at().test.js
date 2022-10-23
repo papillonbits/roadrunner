4ad3f230f378676317655dbf89f7964a300bb68d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-23 17:23:43 +0200

    extend test Array.prototype.concat() tests

--- src/mdn/array/methods/Array.prototype.at().test.js
+++ src/mdn/array/methods/Array.prototype.at().test.js
@@ -1,30 +1,70 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/at
 
 describe('Array', () => {
-  let consoleLogSpy
+  describe('prototype.at()', () => {
+    let consoleLogSpy
 
-  beforeEach(() => {
-    consoleLogSpy = jest.fn(() => {})
-    jest.spyOn(console, 'log').mockImplementation(consoleLogSpy)
-  })
+    beforeEach(() => {
+      consoleLogSpy = jest.fn(() => {})
+      jest.spyOn(console, 'log').mockImplementation(consoleLogSpy)
+    })
 
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
 
-  test('prototype.at()', () => {
-    const array1 = [5, 12, 8, 130, 44]
+    test('Try it', () => {
+      const array1 = [5, 12, 8, 130, 44]
 
-    let index = 2
+      let index = 2
 
-    console.log(`Using an index of ${index} the item returned is ${array1.at(index)}`)
+      console.log(`Using an index of ${index} the item returned is ${array1.at(index)}`)
 
-    expect(consoleLogSpy).toHaveBeenCalledWith('Using an index of 2 the item returned is 8')
+      expect(consoleLogSpy).toHaveBeenCalledWith('Using an index of 2 the item returned is 8')
 
-    index = -2
+      index = -2
 
-    console.log(`Using an index of ${index} item returned is ${array1.at(index)}`)
+      console.log(`Using an index of ${index} item returned is ${array1.at(index)}`)
 
-    expect(consoleLogSpy).toHaveBeenCalledWith('Using an index of -2 item returned is 130')
+      expect(consoleLogSpy).toHaveBeenCalledWith('Using an index of -2 item returned is 130')
+    })
+
+    test('Return the last value of an array', () => {
+      const cart = ['apple', 'banana', 'pear']
+
+      function returnLast(arr) {
+        return arr.at(-1)
+      }
+
+      const item1 = returnLast(cart)
+      expect(item1).toEqual('pear')
+
+      cart.push('orange')
+      const item2 = returnLast(cart)
+      expect(item2).toEqual('orange')
+    })
+
+    test('Comparing methods', () => {
+      const colors = ['red', 'green', 'blue']
+
+      const lengthWay = colors[colors.length - 2]
+      expect(lengthWay).toEqual('green')
+
+      const sliceWay = colors.slice(-2, -1)
+      expect(sliceWay[0]).toEqual('green')
+
+      const atWay = colors.at(-2)
+      expect(atWay).toEqual('green')
+    })
+
+    test('Calling at() on non-array objects', () => {
+      const arrayLike = {
+        length: 2,
+        0: 'a',
+        1: 'b',
+      }
+
+      expect(Array.prototype.at.call(arrayLike, -1)).toEqual('b')
+    })
   })
 })
